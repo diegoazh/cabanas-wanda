05919c0f89d047709b4317595f0a5a61751b76de
Diego A. Zapata Häntsch <diegoazh2003@gmail.com>
2017-04-23 02:02:05 -0300

    frontend profesional

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -5,22 +5,66 @@
 @endsection
 
 @section('content')
-    <hr class="division">
     <section class="row">
         <div class="col-md-8 col-md-offset-2 description">
-            <hr>
-            <img src="{{ asset('images/frontend/cabanas9.jpg') }}" alt="" class="img-responsive img-circle">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquet ipsum id ex blandit sodales. Vestibulum id hendrerit lorem. Ut commodo dui ante, nec mollis magna pharetra a. Nullam pellentesque sagittis ullamcorper. Ut sit amet eros eu nisi tempor elementum. Donec sollicitudin aliquam nibh, ac laoreet elit dapibus mollis. Nullam venenatis justo vitae vestibulum lobortis. Duis non nulla a orci aliquet tincidunt vitae ac nisl. Nulla vitae nibh ac ante commodo luctus nec nec eros. Etiam non turpis sed tortor congue sagittis ac quis urna. Vestibulum vel rutrum mauris. </p>
-        </div>
-    </section>
-    <section class="row">
-        <div class="col-md-4 col-md-offset-2 description-paralel">
-            <img src="{{ asset('images/frontend/cabanas9.jpg') }}" alt="" class="img-responsive img-rounded img-thumbnail">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquet ipsum id ex blandit sodales. Vestibulum id hendrerit lorem. Ut commodo dui ante, nec mollis magna pharetra a. Nullam pellentesque sagittis ullamcorper. Ut sit amet eros eu nisi tempor elementum. Donec sollicitudin aliquam nibh, ac laoreet elit dapibus mollis. Nullam venenatis justo vitae vestibulum lobortis. Duis non nulla a orci aliquet tincidunt vitae ac nisl. Nulla vitae nibh ac ante commodo luctus nec nec eros. Etiam non turpis sed tortor congue sagittis ac quis urna. Vestibulum vel rutrum mauris. </p>
-        </div>
-        <div class="col-md-4 description-paralel">
-            <img src="{{ asset('images/frontend/cabanas9.jpg') }}" alt="" class="img-responsive img-rounded img-thumbnail">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquet ipsum id ex blandit sodales. Vestibulum id hendrerit lorem. Ut commodo dui ante, nec mollis magna pharetra a. Nullam pellentesque sagittis ullamcorper. Ut sit amet eros eu nisi tempor elementum. Donec sollicitudin aliquam nibh, ac laoreet elit dapibus mollis. Nullam venenatis justo vitae vestibulum lobortis. Duis non nulla a orci aliquet tincidunt vitae ac nisl. Nulla vitae nibh ac ante commodo luctus nec nec eros. Etiam non turpis sed tortor congue sagittis ac quis urna. Vestibulum vel rutrum mauris. </p>
+            <div id="main_message" class="row">
+                <div class="col-md-4 col-md-offset-2">
+                    <h2>"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit"</h2>
+                </div>
+                <div class="col-md-6">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vel pharetra libero. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas feugiat condimentum congue. Praesent eleifend sodales justo, a dictum ipsum. Praesent vel gravida massa. Praesent tempor odio sed sem pulvinar pulvinar. Maecenas vulputate risus eu erat condimentum, sit amet auctor ante pretium. Sed tempor nec massa placerat elementum.</p>
+                </div>
+                <div class="col-md-12 text-center">
+                    <div class="link-button" role="button"><a href="#">Lorem ipsum dolor</a></div>
+                </div>
+            </div>
+            <hr class="division">
+            <div id="short_messeges" class="row">
+                <div class="col-md-5 col-md-offset-1">
+                    <div id="first_slogan">
+                        <img src="/images/cabanias/cabanas13.jpg" alt="" class="img-responsive">
+                        <h2>ed quam nulla, faucibus at velit ac, pretium vestibulum erat.</h2>
+                        <p>Cras molestie neque elit, quis gravida elit blandit vel. Fusce leo augue, vehicula vel cursus sed, tristique non massa. Aenean varius laoreet elementum. Nullam suscipit sapien non enim pulvinar rhoncus. Morbi tristique, nunc quis semper dignissim, augue sapien condimentum ante, at interdum tellus massa pretium magna. Cras varius lacus vitae mauris iaculis pretium. Nullam suscipit sapien non enim pulvinar rhoncus.</p>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="col-md-12">
+                        <div id="second_slogan">
+                            <h2>ed quam nulla, faucibus at velit ac, pretium vestibulum erat.</h2>
+                            <p>Cras molestie neque elit, quis gravida elit blandit vel. Fusce leo augue, vehicula vel cursus sed, tristique non massa. Aenean varius laoreet elementum. Nullam suscipit sapien non enim pulvinar rhoncus. Morbi tristique, nunc quis semper dignissim, augue sapien condimentum ante, at interdum tellus massa pretium magna. Cras varius lacus vitae mauris iaculis pretium.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <div id="third_slogan">
+                            <h2>ed quam nulla, faucibus at velit ac, pretium vestibulum erat.</h2>
+                            <p>Cras molestie neque elit, quis gravida elit blandit vel. Fusce leo augue, vehicula vel cursus sed, tristique non massa. Aenean varius laoreet elementum. Nullam suscipit sapien non enim pulvinar rhoncus. Morbi tristique, nunc quis semper dignissim, augue sapien condimentum ante, at interdum tellus massa pretium magna. Cras varius lacus vitae mauris iaculis pretium.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr class="division handicon">
+            <div id="short_messages_2" class="row">
+                <div class="col-md-offset-1 col-md-11">
+                    <div id="fourth_slogan">
+                        <h2>Donec vitae molestie magna. Pellentesque nec odio congue, cursus velit in, lacinia lorem.</h2>
+                        <p>Nunc lobortis ut magna tristique consectetur. Ut maximus ultricies ullamcorper. Etiam a risus lorem. Mauris condimentum ex nunc, eu suscipit lacus tristique at. Donec condimentum dui id porttitor pellentesque. Phasellus maximus non ex et eleifend. Pellentesque facilisis scelerisque turpis sed tristique. Praesent viverra egestas mi. Nam efficitur pulvinar arcu quis egestas. Nunc lobortis ut magna tristique consectetur. Ut maximus ultricies ullamcorper. Etiam a risus lorem. Mauris condimentum ex nunc, eu suscipit lacus tristique at. Donec condimentum dui id porttitor pellentesque. Phasellus maximus non ex et eleifend. Pellentesque facilisis scelerisque turpis sed tristique. Praesent viverra egestas mi. Nam efficitur pulvinar arcu quis egestas. Nunc lobortis ut magna tristique consectetur. Ut maximus ultricies ullamcorper. Etiam a risus lorem. Mauris condimentum ex nunc, eu suscipit lacus tristique at. Donec condimentum dui id porttitor pellentesque. Phasellus maximus non ex et eleifend. Pellentesque facilisis scelerisque turpis sed tristique. Praesent viverra egestas mi. Nam efficitur pulvinar arcu quis egestas.</p>
+                    </div>
+                </div>
+                <div class="col-md-offset-1 col-md-5">
+                    <div id="fifth_slogan">
+                        <img src="/images/cabanias/cabanas5.jpg" alt="" class="img-responsive">
+                        <h2>Donec lobortis sit amet nunc sit amet venenatis.</h2>
+                        <p>Mauris a velit suscipit, malesuada nunc quis, faucibus eros. Aliquam tincidunt nisi id erat dapibus, in vulputate urna consequat. Nam accumsan at lectus sit amet dapibus. Nam eget dui sapien. Nulla ac ligula accumsan, egestas enim ut, ultrices mauris. Donec a eros vel metus lobortis blandit.</p>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div id="sixth_slogan">
+                        <img src="/images/cabanias/cabanas2.jpg" alt="" class="img-responsive">
+                        <h2>Donec lobortis sit amet nunc sit amet venenatis.</h2>
+                        <p>Mauris a velit suscipit, malesuada nunc quis, faucibus eros. Aliquam tincidunt nisi id erat dapibus, in vulputate urna consequat. Nam accumsan at lectus sit amet dapibus. Nam eget dui sapien. Nulla ac ligula accumsan, egestas enim ut, ultrices mauris. Donec a eros vel metus lobortis blandit.</p>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
     <section id="testimonies" class="row">
