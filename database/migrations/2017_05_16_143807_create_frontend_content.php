935b0db551d6f0748fd4fa98774df1362e5ce611
Diego A. Zapata Häntsch <diegoazh2003@gmail.com>
2017-05-16 14:51:57 -0300

    migracion del frontend

---
+++ database/migrations/2017_05_16_143807_create_frontend_content.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFrontendContent extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('frontend_contents', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('tt_presentation');
+            $table->string('msg_presentation');
+            $table->string('txt_btn_presentation');
+            $table->string('lnk_btn_presentation');
+            $table->string('tt_slogan_one');
+            $table->string('desc_slogan_one');
+            $table->string('img_slogan_one');
+            $table->string('tt_slogan_two');
+            $table->string('desc_slogan_two');
+            $table->string('img_slogan_two');
+            $table->string('tt_slogan_three');
+            $table->string('desc_slogan_three');
+            $table->string('img_slogan_three');
+            $table->string('tt_slogan_four')->nullable();
+            $table->string('desc_slogan_four')->nullable();
+            $table->string('img_slogan_four')->nullable();
+            $table->boolean('show_slogan_four');
+            $table->string('tt_slogan_five')->nullable();
+            $table->string('desc_slogan_five')->nullable();
+            $table->string('img_slogan_five')->nullable();
+            $table->boolean('show_slogan_five');
+            $table->string('tt_slogan_six')->nullable();
+            $table->string('desc_slogan_six')->nullable();
+            $table->string('img_slogan_six')->nullable();
+            $table->boolean('show_slogan_six');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('frontend_contents');
+    }
+}
