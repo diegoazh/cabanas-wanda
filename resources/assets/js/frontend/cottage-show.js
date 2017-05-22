ef9c51b1ec83841e5f19c2f7ab2ee79d41ac37d8
Diego A. Zapata Häntsch <diegoazh2003@gmail.com>
2017-05-22 05:23:12 -0300

    corregimos unos problemas con el menú pegajoso en cabañas.

--- resources/assets/js/frontend/cottage-show.js
+++ resources/assets/js/frontend/cottage-show.js
@@ -22,4 +22,23 @@ $(document).ready(function (event) {
             var $table = $('.clndr-table');
         }
     }());
+
+    $(document).ready(function(){
+        var altura = $('.general-menu').offset().top;
+
+        $(window).on('scroll', function(){
+            if ( $(window).scrollTop() > altura ){
+                $('.general-menu').addClass('menu-fixed');
+                $('#arrow_left, #arrow_right').css('top', '15px').css('margin-bottom', '50px');
+                $('#overlay').css('margin-top', '50px');
+                $('.container-logo').css('top', '17.5%');
+            } else {
+                $('.general-menu').removeClass('menu-fixed');
+                $('#arrow_left, #arrow_right').removeAttr('style');
+                $('#overlay').removeAttr('style');
+                $('.container-logo').removeAttr('style');
+            }
+        });
+
+    });
 });
--- resources/assets/js/frontend/cottages-index.js
+++ resources/assets/js/frontend/cottages-index.js
@@ -1,4 +1,23 @@
 $(document).ready(function (event) {
+    $(document).ready(function(){
+        var altura = $('.general-menu').offset().top;
+
+        $(window).on('scroll', function(){
+            if ( $(window).scrollTop() > altura ){
+                $('.general-menu').addClass('menu-fixed');
+                $('#arrow_left, #arrow_right').css('top', '15px').css('margin-bottom', '50px');
+                $('#overlay').css('margin-top', '50px');
+                $('.container-logo').css('top', '17.5%');
+            } else {
+                $('.general-menu').removeClass('menu-fixed');
+                $('#arrow_left, #arrow_right').removeAttr('style');
+                $('#overlay').removeAttr('style');
+                $('.container-logo').removeAttr('style');
+            }
+        });
+
+    });
+
     $('img.img-responsive.img-rounded').click(function (e) {
         e.preventDefault();
         var $padre = $(this).parent('div.col-md-12');
