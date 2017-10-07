5c39d93f8008d85f07c2f8cbfed561f3326d0a18
Diego A. Zapata Häntsch <diegoazh2003@gmail.com>
2017-10-07 19:04:04 -0300

    creamos un seeder para rentals.

---
+++ database/seeds/RentalsTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class RentalsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
