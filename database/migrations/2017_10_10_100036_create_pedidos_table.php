294054a3bf8cb815cdd724bdf63f4016de7d6a6a
Diego A. Zapata Häntsch <diegoazh2003@gmail.com>
2017-10-10 10:10:11 -0300

    Corregimos las migraciones ya que las ultimas dos no se habían registrado correctamente con psr4

---
+++ database/migrations/2017_10_10_100036_create_pedidos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePedidosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pedidos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->dateTime('fecha_pedido');
+            $table->bigInteger('rental_id')->unsigned();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreign('rental_id')->references('id')->on('rentals');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pedidos');
+    }
+}
