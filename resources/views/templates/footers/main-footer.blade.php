63b1f1630e90334b58e552b981e17ad4f302909d
Diego A. Zapata Häntsch <diegoazh2003@gmail.com>
2018-01-01 23:51:38 -0300

    volvimos a modificar el footer para que funcione correctamente.

--- resources/views/templates/footers/main-footer.blade.php
+++ resources/views/templates/footers/main-footer.blade.php
@@ -19,41 +19,61 @@
         </div>
         <div class="col-12 col-md-4 pl-md-5">
             <h2><i class="fa fa-user-circle" aria-hidden="true"></i> Redes Sociales</h2>
-            <a href="@if(isset($content) && !empty($content->facebook)) {{ $content->facebook }} @endif" @if(isset($content) && !empty($content->show_facebook)) @if(!$content->show_facebook) class="hidden" @endif @endif>
-                    <span class="fa-stack fa-lg">
-                      <i class="fa fa-square-o fa-stack-2x"></i>
-                      <i class="fa fa-facebook fa-stack-1x"></i>
-                    </span>
-                ¡Dejanos un me gusta en nuestra fan page!
-            </a>
-            <a href="@if(isset($content) && !empty($content->twitter)) {{ $content->twitter }} @endif" @if(isset($content) && !empty($content->show_twitter)) @if(!$content->show_twitter) class="hidden" @endif @endif>
-                    <span class="fa-stack fa-lg">
-                      <i class="fa fa-square-o fa-stack-2x"></i>
-                      <i class="fa fa-twitter fa-stack-1x"></i>
-                    </span>
-                ¡Siguenos en Twitter!
-            </a>
-            <a href="@if(isset($content) && !empty($content->instagram)) {{ $content->instagram }} @endif" @if(isset($content) && !empty($content->show_instagram)) @if(!$content->show_instagram) class="hidden" @endif @endif>
-                    <span class="fa-stack fa-lg">
-                      <i class="fa fa-square-o fa-stack-2x"></i>
-                      <i class="fa fa-instagram fa-stack-1x"></i>
-                    </span>
-                ¡Siguenos en Instagram!
-            </a>
-            <a href="@if(isset($content) && !empty($content->youtube)) {{ $content->youtube }} @endif" @if(isset($content) && !empty($content->show_youtube)) @if(!$content->show_youtube) class="hidden" @endif @endif>
-                    <span class="fa-stack fa-lg">
-                      <i class="fa fa-square-o fa-stack-2x"></i>
-                      <i class="fa fa-youtube fa-stack-1x"></i>
-                    </span>
-                ¡Suscribete a nuestro canal!
-            </a>
-            <a href="@if(isset($content) && !empty($content->googleplus)) {{ $content->googleplus }} @endif" @if(isset($content) && !empty($content->show_googleplus)) @if(!$content->show_googleplus) class="hidden" @endif @endif>
-                    <span class="fa-stack fa-lg">
-                      <i class="fa fa-square-o fa-stack-2x"></i>
-                      <i class="fa fa-google-plus fa-stack-1x"></i>
-                    </span>
-                ¡Dejanos un +1 en Google+!
-            </a>
+            @if(isset($content) && !empty($content->show_facebook))
+                @if(!$content->show_facebook)
+                    <a href="@if(isset($content) && !empty($content->facebook)) {{ $content->facebook }} @endif">
+                        <span class="fa-stack fa-lg">
+                          <i class="fa fa-square-o fa-stack-2x"></i>
+                          <i class="fa fa-facebook fa-stack-1x"></i>
+                        </span>
+                        ¡Dejanos un me gusta en nuestra fan page!
+                    </a>
+                @endif
+            @endif
+            @if(isset($content) && !empty($content->show_twitter))
+                @if(!$content->show_twitter)
+                    <a href="@if(isset($content) && !empty($content->twitter)) {{ $content->twitter }} @endif">
+                        <span class="fa-stack fa-lg">
+                          <i class="fa fa-square-o fa-stack-2x"></i>
+                          <i class="fa fa-twitter fa-stack-1x"></i>
+                        </span>
+                        ¡Siguenos en Twitter!
+                    </a>
+                @endif
+            @endif
+            @if(isset($content) && !empty($content->show_instagram))
+                @if(!$content->show_instagram)
+                    <a href="@if(isset($content) && !empty($content->instagram)) {{ $content->instagram }} @endif">
+                        <span class="fa-stack fa-lg">
+                          <i class="fa fa-square-o fa-stack-2x"></i>
+                          <i class="fa fa-instagram fa-stack-1x"></i>
+                        </span>
+                        ¡Siguenos en Instagram!
+                    </a>
+                @endif
+            @endif
+            @if(isset($content) && !empty($content->show_youtube))
+                @if(!$content->show_youtube)
+                    <a href="@if(isset($content) && !empty($content->youtube)) {{ $content->youtube }} @endif">
+                        <span class="fa-stack fa-lg">
+                          <i class="fa fa-square-o fa-stack-2x"></i>
+                          <i class="fa fa-youtube fa-stack-1x"></i>
+                        </span>
+                        ¡Suscribete a nuestro canal!
+                    </a>
+                @endif
+            @endif
+            @if(isset($content) && !empty($content->show_googleplus))
+                @if(!$content->show_googleplus)
+                    <a href="@if(isset($content) && !empty($content->googleplus)) {{ $content->googleplus }} @endif">
+                        <span class="fa-stack fa-lg">
+                          <i class="fa fa-square-o fa-stack-2x"></i>
+                          <i class="fa fa-google-plus fa-stack-1x"></i>
+                        </span>
+                        ¡Dejanos un +1 en Google+!
+                    </a>
+                @endif
+            @endif
             <a href="#" id="mail" data-toggle="modal" data-target="#msg_modals_frontend" data-tt-modal="¡Escribenos un e-mail!" data-body-modal="cabaniasdewanda">
                     <span class="fa-stack fa-lg">
                       <i class="fa fa-square-o fa-stack-2x"></i>
