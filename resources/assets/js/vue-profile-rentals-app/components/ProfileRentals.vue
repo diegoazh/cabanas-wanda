f4e362dc16637f5ff30ce946ef5cf98f47fbf87f
Diego A. Zapata Häntsch <diegoazh2003@gmail.com>
2018-01-20 18:10:27 -0300

    corrección de status en dash.vue

--- resources/assets/js/vue-profile-rentals-app/components/ProfileRentals.vue
+++ resources/assets/js/vue-profile-rentals-app/components/ProfileRentals.vue
@@ -1,5 +1,33 @@
 <template>
-
+    <div class="row justify-content-center">
+        <div class="col-12">
+            <table class="table table-striped">
+                <thead class="bg-dark text-light">
+                <tr>
+                    <th>Desde</th>
+                    <th>Hasta</th>
+                    <th>Cabaña</th>
+                    <th>Precio</th>
+                    <th>Acción</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                </tbody>
+                <tfoot>
+                <tr>
+                    <td colspan="5" class="text-right">Reservas ordenadas por {{ orden }}.</td>
+                </tr>
+                </tfoot>
+            </table>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -7,7 +35,9 @@
         name: 'profile-rentals',
         components: {},
         data() {
-            return {}
+            return {
+                orden: 'fecha'
+            }
         },
         computed: {},
         methods: {},
