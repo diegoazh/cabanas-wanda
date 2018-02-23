e36dd179b4d29048ac0bb8e82b61d82738ff130a
diegoazh <diegoazh2003@gmail.com>
2018-02-23 02:57:44 -0300

    Páginas estáticas, listas y con estilos acabados.

--- resources/views/frontend/policies-privacy.blade.php
+++ resources/views/frontend/policies-privacy.blade.php
@@ -8,10 +8,10 @@
 @section('content')
     <div class="row justify-content-center">
         <div class="col-12 col-md-7">
-            <h2 class="text-center bg-dark text-light py-2 rounded"><i class="fa fa-map-signs" aria-hidden="true"></i> Políticas de privacidad</h2>
+            <h2 class="text-center bg-dark text-light py-2 rounded"><i class="fa fa-user-secret" aria-hidden="true"></i> Políticas de privacidad</h2>
             <p class="text-justify">
                 Curabitur tincidunt magna elementum porttitor semper. Etiam vitae lectus ac enim aliquam feugiat quis eget mauris. Aliquam tempus fermentum velit, eget sodales dolor vulputate non. Fusce cursus, dui imperdiet luctus egestas, massa magna dictum quam, ut rhoncus nisi erat fringilla justo. Vestibulum a risus imperdiet, ultricies urna at, eleifend urna. Maecenas sit amet mi venenatis diam cursus rhoncus eu vitae justo. Nam venenatis ante non augue elementum, sit amet dapibus mi tempor. Nam dapibus ante ex, in cursus magna egestas vitae. Proin arcu eros, mollis ac nibh in, tincidunt mattis libero. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque luctus tempus tellus eu bibendum. Donec sodales tortor at massa interdum, vehicula tristique magna tempus. Curabitur dignissim tortor sit amet porttitor vulputate. Sed a aliquet felis.
-                <img src="https://loremflickr.com/320/240?lock=1" />
+                <img src="https://loremflickr.com/g/320/240/paris,girl/all" class="img-responsive img-thumbnail float-left m-3" />
             </p>
             <p class="text-justify">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam erat eros, aliquet ac purus ut, blandit placerat justo. Pellentesque tempus lorem non mauris scelerisque, a tempus mi interdum. Cras venenatis metus non bibendum egestas. Suspendisse et semper diam. Quisque ante urna, bibendum ac rutrum sit amet, consequat at diam. Nulla facilisi. Quisque scelerisque hendrerit leo, eu tincidunt mauris semper in. Morbi porta consectetur est in rhoncus. Morbi posuere accumsan mauris id accumsan. Vestibulum varius vehicula libero, facilisis sagittis magna aliquet faucibus. In lacinia, tellus in dapibus efficitur, erat purus rutrum felis, vel venenatis nulla mauris et felis. Aenean sed hendrerit nulla. Aenean porta enim nec odio convallis, eu ullamcorper diam tincidunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
